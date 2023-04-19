8a3bbf90901a52d1597a6415bc32f16d4b2ff980
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 16:07:39 -0300

    starting studies

---
+++ initial-functions/functionOne.dart
@@ -0,0 +1,6 @@
+void main() {
+  print('Hello World!');
+}
+
+//void main é a função inicial, nossa função principal.
+//void(vazia) function: significa que ela não vai entregar mais nada quando acabar.
