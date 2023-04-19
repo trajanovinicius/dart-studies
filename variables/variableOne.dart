e6c1f4431af389d4702bd3b1cab14e95c491f2a2
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 16:24:49 -0300

    Started variable studies, first integer type

---
+++ variables/variableOne.dart
@@ -0,0 +1,7 @@
+//No dart, existem dois tipos de números: INT e DOUBLE.
+//INT, são inteiros e Doubles, são números fracionados, possui valor depois da vírgula.
+
+void main() {
+  int idade = 26;
+  print(idade);
+}
