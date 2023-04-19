a74a78aac3b7ba78ca7b41821b8527b95e800b11
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 20:38:03 -0300

    completed exercise to apply the concepts of repeating loops

---
+++ exercises/exerciseThree.dart
@@ -0,0 +1,7 @@
+// Criar um laço de repetição com for e printar uma frase no terminal.
+
+void main() {
+  for (int pontos = 5; pontos <= 50; pontos++) {
+    print('você ganhou mais $pontos pontos');
+  }
+}
