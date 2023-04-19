91eca30fdbd64e12df46a6279eac4edbb8e013c6
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 18:40:59 -0300

    understanding the concept of lists in dart

---
+++ lists/listOne.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> nomesAmigos = ['Gabriela', 'Valesca', 'Felipe', 'Higor'];
+  print(nomesAmigos[0]);
+}
+//entre <> eu defini quais valores a lista irá aceitar.
+//chamando o nome da variável e colocando[] podemos acessar dados específicos de acordo com o seu índice.
+//Uma lista ou array, sempre começa do 0.
+//podemos usar o . logo após o nome da lista para acessar as funções disponíveis.
