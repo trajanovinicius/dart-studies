54e81d0d8a4d6c9dd5396d2d2e98f5906d018c5f
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 20:51:32 -0300

    leaving tips for good development practices in dart

---
+++ good-habits/comments.dart
@@ -0,0 +1,6 @@
+// é importante comentar o que queremos e o que estamos fazendo naquele trecho de código.
+//um comentário em dart é feito com: // antes de qualquer coisa que for escrever.
+// usamos /* */: para comentar mais de uma linha de uma vez.
+
+//cuidado, comentar demais causa poluição no código.
+//devemos comentar quando queremos explicar um trecho de código complicado ou deixar uma informação útil para outro dev.
