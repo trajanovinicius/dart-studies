7ab1e9c0a3c5a89cffe9bf3e2c8dbfedcc2f6369
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 16:34:22 -0300

    understanding variables of type double

---
+++ variables/variableTwo.dart
@@ -0,0 +1,8 @@
+//Número tipo DOUBLE:
+void main() {
+  //double altura = 1.80;
+  double altura = 780e6;
+  print(altura);
+}
+//No dart usamos ponto ao invés de vírgula para representar números fracionados.
+//podemos usar exponenciais para representar números grandes.
