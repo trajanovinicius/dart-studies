2b6c3ed80a7495e5274aab72543388a766ba67fd
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 18:54:22 -0300

    understanding the concept of dynamic lists

---
+++ lists/listTwo.dart
@@ -0,0 +1,7 @@
+//Agora vamos criar uma lista dinâmica, com vários dados diferentes
+void main() {
+  List<dynamic> dadosPessoais = [21, 1.80, true, 'Vinicius', 'Torresmo'];
+  print(dadosPessoais);
+}
+//ao colocarmos entre <> a palavra dynamic, dizemos que a lista permite vários dados de tipos diferentes.
+//utilizamos ${} para utilizar dados de uma lista em uma string
