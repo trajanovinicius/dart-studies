5c6722fa75a21778e25ae1dbdfeda43f6260143a
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 19:50:33 -0300

    started the understanding about loops of repetitions.

---
+++ repeating-loops/tiesOne.dart
@@ -0,0 +1,10 @@
+void main() {
+  int idade = 21;
+  bool maiorDeIdade;
+  if (idade >= 18) {
+    maiorDeIdade = true;
+  } else {
+    maiorDeIdade = false;
+  }
+  print('Eu sou maior de idade? $maiorDeIdade');
+}
