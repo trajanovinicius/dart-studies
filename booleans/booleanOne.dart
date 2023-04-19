6b629c1f9e91f71eabba1da1d869ba6e67f13661
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 16:44:32 -0300

    starting the study of boolean values

---
+++ booleans/booleanOne.dart
@@ -0,0 +1,10 @@
+//Boolean é o simples: TRUE ou FALSE que conhecemos.
+
+void main() {
+  int idade = 26;
+  double altura = 1.80;
+  bool geek = (idade ==
+      altura); // aqui fazemos uma comparação, o dart devolve para a gente a respota verdadeira sobre ela.
+  print(geek);
+}
+// definimos uma variável do tipo boolean com a palavra bool antes da declaração do nome.
