c9c2487c03db154335bff17c2a1d7d8fcef370cb
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 17:24:33 -0300

    finished first exercise

---
+++ exercises/exerciseOne.dart
@@ -0,0 +1,15 @@
+/*Criar uma pequenas ficha com minhas informações utilizando conhecimentos em dart
+ e depois imprimilas*/
+
+void main() {
+  int idade = 21;
+  double altura = 1.80;
+  bool geek = true;
+  String nome = "Vinicius";
+  String apelido = "Torresmo";
+
+  print('Eu me chamo $nome'
+      'eu tenho $idade anos,\n'
+      'Minha altura é $altura., \n'
+      'os meus amigos me chamam de $apelido. e eu me considero do mundo geek? $geek');
+}
