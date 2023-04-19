7f5701765b8af9469d2eb603031895705289823b
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-19 17:06:09 -0300

    learning to manipulate strings in dart

---
+++ strings/stringOne.dart
@@ -0,0 +1,8 @@
+void main() {
+  String nome = "Vinicius";
+  String sobrenome = "Trajano";
+  print('Meu é $nome $sobrenome \n e eu sou um engenheiro de software');
+}
+//como nas outras linguagens, utilizar o mais em duas variáveis diferentes, elas são concatenadas.
+//no dart podemos chamar uma variável do tipo string dentro de uma outra string, utilizando $ sozinho ou com chaves.
+//utilizamos \n para quebrar linhas.
